ba216ebff542ee7d26840cfc02da9323c9026c3b
matze328 <152269645+matze328@users.noreply.github.com>
2024-01-22 13:37:36 +0100

    neww

---
+++ alter_volljährig.js
@@ -0,0 +1,10 @@
+const myName = "John";
+ const age = "18"
+
+function überprüfealter(age) {
+    if (age >= 18)
+{console.log(myName + "ist volljährig.")}
+else {console.log(myName + "ist minderjährig")
+}
+}
+
